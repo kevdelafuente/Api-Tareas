59460ef380d5a9efddd475f612d96c7c80a6dd7d
kevdelafuente <soykevindelafuente@gmail.com>
2023-10-07 01:43:58 -0300

    insertar tarea con validacion y su ruta correspondiente

--- app/Http/Controllers/tareaController.php
+++ app/Http/Controllers/tareaController.php
@@ -4,8 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tarea;
+use Illuminate\Support\Facades\Validator;
 
 class tareaController extends Controller
 {
-    //
+    public function InsertarTarea(Request $request){
+        
+        $validation = Validator::make($request->all(), [
+            'titulo' => 'required|string|max:255',
+            'contenido' => 'required|string|max:255',
+            'estado' => 'required|string|max:25',
+            'autor' => 'required|string|min:3|max:255',
+        ]);
+
+        if($validation->fails())
+            return response($validation->errors(),403);
+
+        $tarea=new Tarea();
+        $tarea -> titulo = $request -> post ('titulo');
+        $tarea -> contenido = $request -> post ('contenido');
+        $tarea -> estado = $request -> post ('estado');
+        $tarea -> autor = $request -> post ('autor');
+        $tarea -> save();
+
+        return $tarea;
+    }
 }
--- app/Models/tarea.php
+++ app/Models/tarea.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class tarea extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table="tareas";
+
+    protected $fillable=[
+        "titulo",
+        "contenido",
+        "estado",
+        "autor"
+    ];
 }
